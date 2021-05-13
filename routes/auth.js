3b2ac77951dbc10247c482dfa5d3879db6396f36
fayas-dev <fayas.emedia@gmail.com>
2021-05-13 10:20:00 +0530

    profile get & update password & update user profile apis added

--- routes/auth.js
+++ routes/auth.js
@@ -1,7 +1,11 @@
 const express = require('express');
 const router = express.Router();
+const verifyToken = require("../middlewares/auth");
 const authController = require('../controllers/AuthController');
 router.post('/register', authController.register);
 router.post('/login', authController.login);
+router.get('/profile',verifyToken, authController.profileGet);
+router.put('/profile/update',verifyToken, authController.profileUpdate);
+router.put('/profile/password/update',verifyToken, authController.passwordUpdate);
 
 module.exports = router;
